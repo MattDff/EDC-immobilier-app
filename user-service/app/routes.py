f65152a728d412fe8c7c013dcb9d7c26e2623bf3
MattDff <matt.doffe@gmail.com>
2026-03-02 15:50:59 +0100

    user-serive: add simple test route

--- user-service/app/routes.py
+++ user-service/app/routes.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 bp = Blueprint("users", __name__)
+
+@bp.route("/")
+def home():
+    return jsonify({"message": "API is running"})
