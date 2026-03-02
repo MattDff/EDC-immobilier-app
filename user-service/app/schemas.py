8eab3aa9d7f3b7d915185eafbcaf25948e6f2ec9
MattDff <matt.doffe@gmail.com>
2026-03-02 16:17:49 +0100

    user-serive: add schemas and services

---
+++ user-service/app/schemas.py
@@ -0,0 +1,10 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from app.models import User
+
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        load_instance = True
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
---
+++ user-service/app/services.py
@@ -0,0 +1,31 @@
+from app import db
+from app.models import User
+from sqlalchemy.sql.functions import user
+
+
+def get_all_users():
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    return User.query.get(user_id)
+
+def create_user(data):
+    user = User(
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        date_of_birth=data['birth_date'],
+    )
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+def update_user(data):
+    user.first_name = data.get("firt_name", user.first_name)
+    user.last_name = data.get("last_name", user.last_name)
+    user.birth_date = data.get("birth_date", user.birth_date)
+    db.session.commit()
+    return user
+
+def delete_user(user_id):
+    db.session.delete(user)
+    db.session.commit()
