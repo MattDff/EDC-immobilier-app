42e4662c869d771fdbc9cba6c118d30d1e25a58e
MattDff <matt.doffe@gmail.com>
2026-03-03 12:37:07 +0100

    property-service: add __init__.py

---
+++ property-service/app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    from config import Config
+    app.config.from_object(Config)
+
+    if test_config:
+        app.config.update(test_config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .routes import bp
+    app.register_blueprint(bp, url_prefix="/api/v1")
+
+    return app
